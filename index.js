76434834b323f11eb2b977ae33c9054a541f6c6e
KonvictEpic <epic.bull@hotmail.com>
2020-07-14 15:07:16 +0200

    bot is working for intended purpose, maybe add more functionality in the future

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ client.once('ready', () => {
 
 client.login(config.token);
 client.on('message', message => {
+	const channel = 'welcome-room';
+/*	console.log(message.content);
+	console.log(message.channel.name);
+	message.delete();
+*/
+ // if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-  if (message.content === '$understood' || message.content === '!understood') {
-	message.channel.send('Pong.');
+// const args = message.content.slice(config.prefix.lenght).split(' ');
+// const command = args.shift().toLowerCase();
+	if (message.content === '$understood' || message.content === '!understood') {
+		return;
+	}
+else if (channel === message.channel.name) {
+message.delete().catch(err=> {
+	console.error(err);
+
+});
 }
 });
