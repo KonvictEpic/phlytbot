5af7aa9b698f14279f1b88b10eadc6374ab6140e
KonvictEpic <epic.bull@hotmail.com>
2020-08-03 17:13:55 +0200

    fixed security issue with mute work around

--- index.js
+++ index.js
@@ -10,31 +10,37 @@ client.once('ready', () => {
 client.login(config.token);
 client.on('message', message => {
 	const channel = 'welcome-room';
+	const guild = client.guilds.cache.get('334839197607264257');
 /*	console.log(message.content);
 	console.log(message.channel.name);
 	message.delete();
 */
-const args = message.content.split(/ +/);
+	const args = message.content.split(/ +/);
 
 // if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
+/*
 if (message.content === '$understood' || message.content === '!understood' || message.author.bot) {
 		return;
 	}
-else if (channel === message.channel.name) {
-message.delete().catch(err=> {
-	console.error(err);
+	*/
+	if (channel === message.channel.name) {
 
-});
-}
-else if (args[0] === '!mute') {
-const guild = client.guilds.cache.get('334839197607264257');
+		if (!message.content === '$understood' || !message.content === '!understood' || !message.author.bot) {
+			message.delete().catch(err=> {
+			console.error(err);
+			});
+		}
+	}
+	else if (message.member.roles.cache.has('334841054656462849') || message.member.roles.cache.has('435696785772773376')) {
+		if (args[0] === '!mute') {
 
-const member = guild.members.cache.get(args[1]);
-member.roles.remove('710237948150284389').catch(err => {
-	console.error(err);
-});
-}
+		const member = guild.members.cache.get(args[1]);
+		member.roles.remove('710237948150284389').catch(err => {
+			console.error(err);
+			});
+		}
+	}
+	});
 // console.log(message.mentions.users.first());
 /* taggedUser.id.roles.remove(710237948150284389).catch(err=> {
 	console.error(err);
@@ -56,5 +62,3 @@ console.log(message.mentions.users.first());
 	}
 }
 */
-
-});
